73439347d015a4c6c0b856d1450ee882952c36f7
bakgom123 <gunjudavid@naver.com>
2024-03-29 17:00:26 +0900

    solved

---
+++ lv1/서울에서 김서방 찾기.py
@@ -0,0 +1,7 @@
+def solution(seoul):
+#     idx = 0
+#     for kim in seoul:
+#         if kim == "Kim":
+#             return f"김서방은 {idx}에 있다"
+#         idx += 1
+    return "김서방은 {}에 있다".format(seoul.index('Kim'))
