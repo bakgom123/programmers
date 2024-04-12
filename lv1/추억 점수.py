afedc325a3c25a6b949566d1ec958fe17cd19ea5
bakgom123 <gunjudavid@naver.com>
2024-04-12 21:41:45 +0900

    solved

---
+++ lv1/추억 점수.py
@@ -0,0 +1,9 @@
+def solution(name, yearning, photo):
+    answer = []
+    for pic in photo:
+        score = 0
+        for i in range(len(name)):
+            score += pic.count(name[i]) * yearning[i]
+        answer.append(score)
+        
+    return answer
