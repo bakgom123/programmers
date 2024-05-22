4deafcd4318161f381f43910740f7162796dd02a
bakgom123 <gunjudavid@naver.com>
2024-05-22 15:21:51 +0900

    solved

---
+++ JavaScript/lv1/같은 숫자는 싫어.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+  let answer = [];
+  for (let i of arr) {
+    if (answer.length === 0 || answer[answer.length - 1] !== i) {
+      answer.push(i);
+    }
+  }
+  return answer;
+}
